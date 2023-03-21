c9847b4c39a16f4de7141290027dabdd307aed64
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 13:34:33 -0500

    feat: :sparkles: v-bind(Objetos)
    Utilizacion de la directiva v-bind(Objetos)

--- component/my-component.js
+++ component/my-component.js
@@ -1,10 +1,10 @@
 export default {
     data() {
         return {
-            atributo: 'src',
-            imagen: 'https:/www.futbolred.com/files/article_main/files/crop/uploads/2021/07/01/60de2adaa1555.r_1625172710679.0-344-720-704.jpeg',
-            atributo2: 'alt',
+            imagen:{
+            src: 'https:/www.futbolred.com/files/article_main/files/crop/uploads/2021/07/01/60de2adaa1555.r_1625172710679.0-344-720-704.jpeg',
             alt: 'messiento libre'
+        }
         }
     }
 }
