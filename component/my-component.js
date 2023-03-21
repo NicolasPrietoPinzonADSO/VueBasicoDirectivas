8e48b2ae1c98c139f2539bb23446f9510ba186dd
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 13:40:58 -0500

    feat: :sparkles: class
    Utilizacion de la directiva de class

--- component/my-component.js
+++ component/my-component.js
@@ -1,10 +1,13 @@
 export default {
     data() {
         return {
-            imagen:{
-            src: 'https:/www.futbolred.com/files/article_main/files/crop/uploads/2021/07/01/60de2adaa1555.r_1625172710679.0-344-720-704.jpeg',
-            alt: 'messiento libre'
-        }
+            className:'box',
+            className2: ['box','box2'],
+            className3:{
+                box:true,
+                box2:false,
+                box3:true
+            }
         }
     }
 }
