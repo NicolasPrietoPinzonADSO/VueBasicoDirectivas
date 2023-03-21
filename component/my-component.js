edff83688f2aedbbb498de4bb6e96f5c94ed2fbf
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 13:45:46 -0500

    feat: :sparkles: :style
    Utilizacion de la directiva style

--- component/my-component.js
+++ component/my-component.js
@@ -1,13 +1,18 @@
 export default {
     data() {
         return {
-            className:'box',
-            className2: ['box','box2'],
-            className3:{
-                box:true,
-                box2:false,
-                box3:true
+            styleName: "background: red",
+            styleName2: ['background: #a1a1a1', 'color:#fff'],
+            styleName3:{
+                background: 'violet',
+                color: '#fff',
+                'font-size': '2rem',
+                'font-family': 'sans-serif',
+                padding: '1em',
+                'border-radius': '.5rem',
+                margin: '.5rem 0'
             }
+
         }
     }
 }
