ae35db5ad017543ac9e05bb3153b8ee08482b25e
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 10:56:11 -0500

    feat: :sparkles: v-model
    Utilizacion de la directiva v-model

--- component/my-component.js
+++ component/my-component.js
@@ -1,9 +1,7 @@
 export default{
     data(){
         return{
-            basico : `<strong style="color:green;">Apolo</strong>`,
-            intermedio : `<strong style="color:green;">Artemis</strong>`,
-            avanzado : `<strong style="color:blue;">Sputnik</strong>`
+            mensaje: 'holamundo'
         }
     }
 }
