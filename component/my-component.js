8afe42709af7111ba688f7b3b415385b5541dcca
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 11:14:40 -0500

    feat: :sparkles: v-if, v-else-if, v-else
    Utilizacion de las directivas v-if, v-else-if y v-else

--- component/my-component.js
+++ component/my-component.js
@@ -1,7 +1,11 @@
-export default{
-    data(){
-        return{
-            centinela: false
+export default {
+    data() {
+        return {
+            mensaje: '<em>Apolo</em>',
+            mensaje2: '<mark>Artemis</mark>',
+            mensaje3: '<u>Sputnik</u>',
+            mensaje4: 'no cumple la condicion',
+            opcion: null
         }
     }
 }
