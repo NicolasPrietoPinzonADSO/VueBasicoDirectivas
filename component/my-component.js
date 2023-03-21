27da88f4d970054b2d7af90bc916eb95c0c397ed
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 13:31:38 -0500

    feat: :sparkles: v-bind(dinamico)
    Utilizacion de la directiva v-bind(Dinamico)

--- component/my-component.js
+++ component/my-component.js
@@ -1,7 +1,9 @@
 export default {
     data() {
         return {
-            imagen: 'https://www.futbolred.com/files/article_main/files/crop/uploads/2021/07/01/60de2adaa1555.r_1625172710679.0-344-720-704.jpeg',
+            atributo: 'src',
+            imagen: 'https:/www.futbolred.com/files/article_main/files/crop/uploads/2021/07/01/60de2adaa1555.r_1625172710679.0-344-720-704.jpeg',
+            atributo2: 'alt',
             alt: 'messiento libre'
         }
     }
