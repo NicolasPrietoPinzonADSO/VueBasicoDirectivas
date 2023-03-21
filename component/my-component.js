fdea23169c874ddd5500560694fcfeae3bf8cddb
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-03-21 13:26:54 -0500

    feat: :sparkles: v-bind
    Utilizacion de la directiva v-bind(Pura)

--- component/my-component.js
+++ component/my-component.js
@@ -1,11 +1,8 @@
 export default {
     data() {
         return {
-            lista:[
-                "Apolo",
-                "Artemis",
-                "Sputnik"
-            ]
+            imagen: 'https://www.futbolred.com/files/article_main/files/crop/uploads/2021/07/01/60de2adaa1555.r_1625172710679.0-344-720-704.jpeg',
+            alt: 'messiento libre'
         }
     }
 }
